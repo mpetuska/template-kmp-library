e26a5f70917e0f05b58fff8a54a85aac5be190b1
mpetuska <martynas@petuska.dev>
2021-06-19 13:30:09 +0100

    Kotlin 1.5.30-dev-2423

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -6,13 +6,17 @@ repositories {
   gradlePluginPortal()
   mavenCentral()
   google()
+  if (project.properties["useSnapshotRepositories"] == "true") {
+    maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
+  }
 }
 
 dependencies {
   implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:_")
 //  implementation("com.android.library:com.android.library.gradle.plugin:_")
   implementation("org.jetbrains.dokka:dokka-gradle-plugin:_")
-  implementation("org.jetbrains.kotlin:kotlin-serialization:_")
+  //TODO Revert to `_` placeholder
+  implementation("org.jetbrains.kotlin:kotlin-serialization:1.5.20-RC")
   implementation("org.jlleitschuh.gradle:ktlint-gradle:_")
   implementation("io.github.gradle-nexus:publish-plugin:_")
 }
