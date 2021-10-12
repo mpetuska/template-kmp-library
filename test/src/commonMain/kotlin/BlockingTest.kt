b038937fa1bae673de6f58591ae206169daaeb25
Martynas Petuška <martynas@petuska.dev>
2021-10-12 20:45:25 +0100

    Version bump and cleanup

--- test/src/commonMain/kotlin/BlockingTest.kt
+++ test/src/commonMain/kotlin/BlockingTest.kt
@@ -7,7 +7,7 @@ internal expect fun runBlockingTest(test: suspend CoroutineScope.() -> Unit)
 interface BlockingTest {
   suspend fun beforeEach() {}
   suspend fun afterEach() {}
-  
+
   fun blockingTest(action: suspend CoroutineScope.() -> Unit) = runBlockingTest {
     try {
       beforeEach()
