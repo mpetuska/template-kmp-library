f241ccaeb58ecf662693c85974f52f08ccf9a1af
Martynas Petuška <martynas@petuska.dev>
2023-04-30 20:37:47 +0100

    control fixes

--- build-conventions/src/main/kotlin/convention.kotlin-mpp.gradle.kts
+++ build-conventions/src/main/kotlin/convention.kotlin-mpp.gradle.kts
@@ -1,4 +1,10 @@
+import Env.CI
+import Env.SANDBOX
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeCompilation
+import util.buildHost
+import util.enabled
+import util.mainHost
 
 plugins {
   id("convention.base")
@@ -8,4 +14,21 @@ plugins {
 kotlin {
   @OptIn(ExperimentalKotlinGradlePluginApi::class)
   targetHierarchy.default()
+
+  targets.all target@{
+    val control = Action<Task> {
+      onlyIf {
+        !CI || this@target.enabled || (SANDBOX && this@target.buildHost == mainHost)
+      }
+    }
+    compilations.all compilation@{
+      compileTaskProvider.configure(control)
+
+      if (this@compilation is KotlinNativeCompilation) {
+        this@compilation.cinterops.all {
+          tasks.named(interopProcessingTaskName, control)
+        }
+      }
+    }
+  }
 }
--- build-conventions/src/main/kotlin/convention.publishing-mpp.gradle.kts
+++ build-conventions/src/main/kotlin/convention.publishing-mpp.gradle.kts
@@ -1,7 +1,6 @@
 import Env.CI
 import Env.SANDBOX
 import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinAndroidTarget
-import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeCompilation
 import org.jetbrains.kotlin.konan.target.HostManager
 import util.buildHost
 import util.enabled
@@ -29,20 +28,10 @@ kotlin {
   }
 
   targets.all target@{
-    val control = Action<Task> {
+    configurePublishTasks(this@target.name) {
       onlyIf {
         !CI || this@target.enabled || (SANDBOX && this@target.buildHost == mainHost)
       }
-    }
-    compilations.all compilation@{
-      compileTaskProvider.configure(control)
-
-      if (this@compilation is KotlinNativeCompilation) {
-        this@compilation.cinterops.all {
-          tasks.named(interopProcessingTaskName, control)
-        }
-      }
-      configurePublishTasks(this@target.name, control)
     }
 
     if (this@target is KotlinAndroidTarget && (!CI || this@target.enabled || SANDBOX))
